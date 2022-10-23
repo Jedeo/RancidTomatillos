5e55e51f40556bb24620280ac99c359e5298f808
Connor Corcoran <connorcorc@gmail.com>
2022-10-23 14:16:56 -0600

    Fix swiper

--- rancid-tomatillos/src/components/Movie.jsx
+++ rancid-tomatillos/src/components/Movie.jsx
@@ -1,15 +1,35 @@
 import React from "react";
 import MovieCard from "./MovieCard";
 import "./Movie.css"
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Navigation, Mousewheel, Keyboard, Scrollbar, A11y } from "swiper";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 
-const Movie = ({ error, errorMessage, movies}) => {
+
+const Movie = ({ loading, error, errorMessage, movies, handleClick }) => {
+  const slidesLength = (movies.length < 4 && movies.length > 0 ? movies.length : 4);
   const moviesData = movies.map((movie, index) => (
-    <MovieCard  key={index}  movieInfo={movie} />
+    <SwiperSlide>
+    <MovieCard  key={index} handleClick={handleClick} movieInfo={movie} loading={loading} />
+    </SwiperSlide>
+
   ));
   return (
     <div className="movieData">
       {error === true && <h1 className="errorHeader">Error! {errorMessage}. Please Try Again</h1>}
+      <Swiper
+      modules={[Navigation, Pagination, Scrollbar, A11y]}
+      spaceBetween={100}
+      style={{margin: 20}}
+      slidesPerView={slidesLength}
+      navigation
+      onSwiper={(swiper) => console.log(swiper)}
+        >
       {moviesData}
+      </Swiper>
     </div>
   );
 };
