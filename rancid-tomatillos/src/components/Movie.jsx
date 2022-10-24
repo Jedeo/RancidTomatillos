8d85605d97fc18d432ab9b211eaeedf4461a3aba
Jedeo Manirikumwenatwe <jedeopastor@gmail.com>
2022-10-24 14:21:26 -0600

    Func: passing down props to details

--- rancid-tomatillos/src/components/Movie.jsx
+++ rancid-tomatillos/src/components/Movie.jsx
@@ -1,34 +1,42 @@
 import React from "react";
 import MovieCard from "./MovieCard";
-import "./Movie.css"
+import "./Movie.css";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination, Navigation, Mousewheel, Keyboard, Scrollbar, A11y } from "swiper";
+import {
+  Pagination,
+  Navigation,
+  Mousewheel,
+  Keyboard,
+  Scrollbar,
+  A11y,
+} from "swiper";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
 
-
-const Movie = ({ loading, error, errorMessage, movies, handleClick }) => {
-  const slidesLength = (movies.length < 4 && movies.length > 0 ? movies.length : 4);
+const Movie = ({ loading, error, errorMessage, movies }) => {
+  const slidesLength =
+    movies.length < 4 && movies.length > 0 ? movies.length : 4;
   const moviesData = movies.map((movie) => (
     <SwiperSlide key={movie.id}>
-    <MovieCard  key={movie.id} handleClick={handleClick} movieInfo={movie} loading={loading} />
+      <MovieCard key={movie.id} movieInfo={movie} loading={loading} />
     </SwiperSlide>
-
   ));
   return (
     <div className="movieData">
-      {error === true && <h1 className="errorHeader">Error! {errorMessage}. Please Try Again</h1>}
+      {error === true && (
+        <h1 className="errorHeader">Error! {errorMessage}. Please Try Again</h1>
+      )}
       <Swiper
-      modules={[Navigation, Pagination, Scrollbar, A11y]}
-      spaceBetween={100}
-      style={{margin: 20}}
-      slidesPerView={slidesLength}
-      navigation
-      onSwiper={(swiper) => console.log(swiper)}
-        >
-      {moviesData}
+        controls={true}
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        spaceBetween={100}
+        style={{ margin: 20 }}
+        slidesPerView={slidesLength}
+        navigation
+      >
+        {moviesData}
       </Swiper>
     </div>
   );
--- rancid-tomatillos/src/components/MovieDetails.jsx
+++ rancid-tomatillos/src/components/MovieDetails.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import Details from "./Details";
 import './MovieDetails.css'
 
-const MovieDetails = ({details,movieId}) => {
+const MovieDetails = ({details,movieId, getError}) => {
+        
     return (
         <div className="movieDetailsContainer">
-            <Details movieId={movieId} details={details}/>
+            <Details movieId={movieId} details={details} getError={getError}/>
         </div>
     )
 }
